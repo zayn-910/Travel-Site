c2f21fc4d8138b1bffa9877ee0ecf7e794e27d9a
Zenul Aabedeen Khan <zaynaadi667@gmail.com>
2026-08-24 21:08:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -237,7 +237,7 @@ function renderNavBar() {
     '<nav class="navbar">' +
       '<div class="container nav-inner">' +
         '<div class="brand" data-nav="home">' +
-          '<img src="1.jpg.jpeg" alt="Joy Makers Holidays Logo" class="brand-logo" onerror="this.onerror=null;this.src=window.LOGO_FALLBACK;">' +
+          '<img src="1.jpg.svg" alt="Joy Makers Holidays Logo" class="brand-logo" onerror="this.onerror=null;this.src=window.LOGO_FALLBACK;">' +
           '<div class="brand-text">' +
             '<span class="brand-name">Joy Makers</span>' +
             '<span class="brand-sub">Holidays</span>' +
@@ -812,7 +812,7 @@ function renderFooter() {
         '<div class="footer-grid">' +
           '<div>' +
             '<div class="footer-brand">' +
-              '<img src="1.jpg.jpeg" alt="Joy Makers Holidays Logo" onerror="this.onerror=null;this.src=window.LOGO_FALLBACK;" />' +
+              '<img src="1.jpg.svg" alt="Joy Makers Holidays Logo" onerror="this.onerror=null;this.src=window.LOGO_FALLBACK;" />' +
               '<div class="flex flex-col">' +
                 '<span class="footer-brand-name">Joy Makers</span>' +
                 '<span class="footer-brand-sub">Holidays</span>' +
